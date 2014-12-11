a31fc9161b74a49e97d1c48bc89e506abe5dc87f
Ken Wilcox <wilcoxk@gmail.com>
2014-12-11 13:23:06 -0700

    Added IRange

---
+++ CC.Common.Parser/IRange.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CC.Common.Parser
+{
+  public interface IRange<T> where T : IComparable<T>
+  {
+    T Start { get; }
+    T End { get; }
+    bool Contains(T valueToFind);
+  }
+}
