5fc5e69c8b826b52555fd56fd2c33acd8e6334fc
Ken Wilcox <wilcoxk@gmail.com>
2014-12-11 13:24:14 -0700

    Added Generic Range Class

---
+++ CC.Common.Parser/Range.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CC.Common.Parser
+{
+public class Range<T> : IRange<T> where T : IComparable<T>
+  {
+    private readonly T start;
+    private readonly T end;
+
+    public Range(T start, T end)
+    {
+      if (start.CompareTo(end) <= 0)
+      {
+        this.start = start;
+        this.end = end;
+      }
+      else
+      {
+        this.start = end;
+        this.end = start;
+      }
+    }
+
+    public T Start
+    {
+      get { return this.start; }
+    }
+
+    public T End
+    {
+      get { return this.end; }
+    }
+
+    public bool Contains(T valueToFind)
+    {
+      return valueToFind.CompareTo(Start) >= 0 && valueToFind.CompareTo(End) <= 0;
+    }
+}
